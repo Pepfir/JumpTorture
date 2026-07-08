95d07ad96e586662f06f44939fe02705636f282e
Pepfir <dikoyt3@gmail.com>
2026-07-08 19:30:28 +0300

    Update JumpLayer.hpp

--- src/JumpLayer.hpp
+++ src/JumpLayer.hpp
@@ -29,8 +29,8 @@ protected:
 
     virtual void registerWithTouchDispatcher() override;
     
-    // ВОТ ЗДЕСЬ ИЗМЕНЕНИЕ: мы добавили "double timestamp"
-    virtual void keyDown(enumKeyCodes key) override;
+    // Вот здесь убрали "override", из-за этого была ошибка
+    void keyDown(enumKeyCodes key);
     
     virtual bool ccTouchBegan(CCTouch* touch, CCEvent* event) override;
     virtual void keyBackClicked() override;
